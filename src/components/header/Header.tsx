3247454c154251334061fdee183a9af20409ba46
GarretHawke <alexandra17au@gmail.com>
2021-07-07 23:22:03 +0300

    feat: change paths

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,17 +8,17 @@ function Header(): JSX.Element {
   const [menuActive, setMenuActive] = useState(false);
   const [trainActive, setTrainActive] = useState(true);
 
-  const initialPath = '/menu/';
+  const initialPath = './menu/';
   const items = [
-    {name: 'Main page', href: '/', image: `${initialPath}main-icon.png`, id: 1},
-    {name: 'Action (Set A)', href: '/category-1', image: `${initialPath}action1-icon.png`, id: 2},
-    {name: 'Action (Set B)', href: '/category-2', image: `${initialPath}/action2-icon.png`, id: 3},
-    {name: 'Animal (Set A)', href: '/category-3', image: `${initialPath}animal1-icon.png`, id: 4},
-    {name: 'Animal (Set B)', href: '/category-4', image: `${initialPath}animal2-icon.png`, id: 5},
-    {name: 'Clothes', href: '/category-5', image: `${initialPath}clothes-icon.png`, id: 6},
-    {name: 'Emotions', href: '/category-6', image: `${initialPath}emotions-icon.png`, id: 7},
-    {name: 'Food', href: '/category-7', image: `${initialPath}food-icon.png`, id: 8},
-    {name: 'Travelling', href: '/category-8', image: `${initialPath}travelling-icon.png`, id: 9}
+    {name: 'Main page', href: '/', image: `${initialPath}main.png`, id: 1},
+    {name: 'Action (Set A)', href: '/category-1', image: `${initialPath}action1.png`, id: 2},
+    {name: 'Action (Set B)', href: '/category-2', image: `${initialPath}action2.png`, id: 3},
+    {name: 'Animal (Set A)', href: '/category-3', image: `${initialPath}animal1.png`, id: 4},
+    {name: 'Animal (Set B)', href: '/category-4', image: `${initialPath}animal2.png`, id: 5},
+    {name: 'Clothes', href: '/category-5', image: `${initialPath}clothes.png`, id: 6},
+    {name: 'Emotions', href: '/category-6', image: `${initialPath}emotions.png`, id: 7},
+    {name: 'Food', href: '/category-7', image: `${initialPath}food.png`, id: 8},
+    {name: 'Travelling', href: '/category-8', image: `${initialPath}travelling.png`, id: 9}
   ]
 
   return(
