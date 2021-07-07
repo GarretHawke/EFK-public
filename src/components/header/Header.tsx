99a3aafbe2a32fd82a56e8b677fd8fd4bd4a53a6
GarretHawke <alexandra17au@gmail.com>
2021-07-07 21:51:43 +0300

    feat: change paths

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,15 +8,15 @@ function Header(): JSX.Element {
   const [menuActive, setMenuActive] = useState(false);
   const [trainActive, setTrainActive] = useState(true);
   const items = [
-    {name: 'Main page', href: '/', image: 'main-icon.png', id: 1},
-    {name: 'Action (Set A)', href: '/category-1', image: 'action1-icon.png', id: 2},
-    {name: 'Action (Set B)', href: '/category-2', image: 'action2-icon.png', id: 3},
-    {name: 'Animal (Set A)', href: '/category-3', image: 'animal1-icon.png', id: 4},
-    {name: 'Animal (Set B)', href: '/category-4', image: 'animal2-icon.png', id: 5},
-    {name: 'Clothes', href: '/category-5', image: 'clothes-icon.png', id: 6},
-    {name: 'Emotions', href: '/category-6', image: 'emotions-icon.png', id: 7},
-    {name: 'Food', href: '/category-7', image: 'food-icon.png', id: 8},
-    {name: 'Travelling', href: '/category-8', image: 'travelling-icon.png', id: 9}
+    {name: 'Main page', href: '/', image: './menu/main-icon.png', id: 1},
+    {name: 'Action (Set A)', href: '/category-1', image: './menu/action1-icon.png', id: 2},
+    {name: 'Action (Set B)', href: '/category-2', image: './menu/action2-icon.png', id: 3},
+    {name: 'Animal (Set A)', href: '/category-3', image: './menu/animal1-icon.png', id: 4},
+    {name: 'Animal (Set B)', href: '/category-4', image: './menu/animal2-icon.png', id: 5},
+    {name: 'Clothes', href: '/category-5', image: './menu/clothes-icon.png', id: 6},
+    {name: 'Emotions', href: '/category-6', image: './menu/emotions-icon.png', id: 7},
+    {name: 'Food', href: '/category-7', image: './menu/food-icon.png', id: 8},
+    {name: 'Travelling', href: '/category-8', image: './menu/travelling-icon.png', id: 9}
   ]
 
   return(
