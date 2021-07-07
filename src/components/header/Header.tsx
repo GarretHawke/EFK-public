dee5d4003e3ea790766c07e74c6345ecb826ac1f
GarretHawke <alexandra17au@gmail.com>
2021-07-07 23:04:18 +0300

    feat: change path

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,16 +7,18 @@ import Burger from './Burger';
 function Header(): JSX.Element {
   const [menuActive, setMenuActive] = useState(false);
   const [trainActive, setTrainActive] = useState(true);
+
+  const initialPath = '/menu/';
   const items = [
-    {name: 'Main page', href: '/', image: './menu/main-icon.jpg', id: 1},
-    {name: 'Action (Set A)', href: '/category-1', image: './menu/action1-icon.png', id: 2},
-    {name: 'Action (Set B)', href: '/category-2', image: './menu/action2-icon.jpg', id: 3},
-    {name: 'Animal (Set A)', href: '/category-3', image: './menu/animal1-icon.jpg', id: 4},
-    {name: 'Animal (Set B)', href: '/category-4', image: './menu/animal2-icon.jpg', id: 5},
-    {name: 'Clothes', href: '/category-5', image: './menu/clothes-icon.jpg', id: 6},
-    {name: 'Emotions', href: '/category-6', image: './menu/emotions-icon.jpg', id: 7},
-    {name: 'Food', href: '/category-7', image: './menu/food-icon.jpg', id: 8},
-    {name: 'Travelling', href: '/category-8', image: './menu/travelling-icon.jpg', id: 9}
+    {name: 'Main page', href: '/', image: `${initialPath}main-icon.png`, id: 1},
+    {name: 'Action (Set A)', href: '/category-1', image: `${initialPath}action1-icon.png`, id: 2},
+    {name: 'Action (Set B)', href: '/category-2', image: `${initialPath}/action2-icon.png`, id: 3},
+    {name: 'Animal (Set A)', href: '/category-3', image: `${initialPath}animal1-icon.png`, id: 4},
+    {name: 'Animal (Set B)', href: '/category-4', image: `${initialPath}animal2-icon.png`, id: 5},
+    {name: 'Clothes', href: '/category-5', image: `${initialPath}clothes-icon.png`, id: 6},
+    {name: 'Emotions', href: '/category-6', image: `${initialPath}emotions-icon.png`, id: 7},
+    {name: 'Food', href: '/category-7', image: `${initialPath}food-icon.png`, id: 8},
+    {name: 'Travelling', href: '/category-8', image: `${initialPath}travelling-icon.png`, id: 9}
   ]
 
   return(
